5790b402648b1e71ba652282ebbf6f2c38eca99d
Flav3l <flav3l@pop-os.localdomain>
2023-09-13 14:28:10 -0700

    so close

--- index.js
+++ index.js
@@ -3,29 +3,48 @@ console.log('My code is running');
 
 
 let inputField = document.querySelector(".userInput");
-let SubmitButton = document.querySelector('.submit');
+let submitButton = document.querySelector('.submit');
 let listItems = document.querySelector('.itemList');
-let banana = document.querySelectorAll('li');
-
+let deleteButton = document.querySelector('.delete');
 
 let submit = () => {
     if (inputField.value != "") {
-    listItems.innerHTML += `<li class="unselected">${inputField.value}</li>`
-    inputField.value = "";
+        listItems.innerHTML += `<li class="unselected">${inputField.value}<button class="delete">Delete</button></li>`;
+        
+        document.querySelectorAll('li').forEach(li => {
+            li.addEventListener('click', () => strikeThrough(li));
+            document.querySelectorAll('.delete').forEach(dButton => { 
+                dButton.addEventListener('click', () => deleteFinished(li)) 
+            });
+    })
+inputField.value = "";
     }
 }
 
+
+
 inputField.addEventListener('keypress', event => {
-    if(event.key == 'Enter'){
+    if (event.key == 'Enter') {
         submit();
     }
 });
 
-SubmitButton.addEventListener('click', submit)
 
 
+let strikeThrough = (item) => {
+    if (!item.style.textDecoration || item.style.textDecoration == 'none')//element is not already selected
+    {
+        item.style.textDecoration = 'line-through'//select element and apply strikethrough style
+    }
+    else if (item.style.textDecoration == 'line-through') //element is already selected
+    {
+        item.style.textDecoration = 'none'//unselect element
+    }
+}
 
- listItems.addEventListener('click').style.textDecoration = "line-through";
-    
+let deleteFinished = (item) => {
+    item.remove();//deletes items with a strikethrough
+}
 
-
+submitButton.addEventListener('click', submit);
+deleteButton.addEventListener('click', deleteFinished);
