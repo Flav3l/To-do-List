bcf67f89ba670624f14efe132084db67e91b776c
Flav3l <flav3l@pop-os.localdomain>
2023-09-13 08:35:11 -0700

    first save

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('My code is running');
+
+// var enterPress =  () => {
+//     document.addEventListener('keypress', function(Event) => {
+//         if(Event.key === 13){
+//             return "enter key pressed";
+//         }
+//     });
+// }
+// console.log(enterPress)
+
