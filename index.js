4455e6b1022e55bc093c5520b1eefd4b658f5629
Flav3l <flav3l@pop-os.localdomain>
2023-09-13 08:52:40 -0700

    fixed event listener

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 console.log('My code is running');
 
-// var enterPress =  () => {
-//     document.addEventListener('keypress', function(Event) => {
-//         if(Event.key === 13){
-//             return "enter key pressed";
-//         }
-//     });
-// }
-// console.log(enterPress)
+let inputField = document.querySelector(".userInput");
 
+inputField.addEventListener('keypress', event => {
+    if(event.key == 'Enter'){
+        console.log("enter key pressed");
+    }
+});
+
+// var enterPress =  () => {
+    
+// }
+// console.log(enterPress())
+
+var currentItem = "";
